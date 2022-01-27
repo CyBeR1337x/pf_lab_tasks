409651eb55dbbf45431fc8bc913c24ae3577003d
Syed Shah Meer Haider <60551634+CyBeR1337x@users.noreply.github.com>
2022-01-28 00:31:31 +0500

    Create EXERCISE2(PAGE_81).cpp

---
+++ EXERCISE2(PAGE_81).cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void main() {
+
+  int array[10];
+	cout << "Enter 10 values: ";
+	for (int i = 0; i < 10; i++)
+		cin >> array[i];
+
+	for (int i = 9; i >= 1; i--) {
+		int max = 0;
+		for (int j = 1; j <= i; j++)
+			if (array[max] < array[j])
+				array = j;
+		int temp;
+		temp = array[max];
+		array[max] = array[i];
+		array[i] = temp;
+	}
+
+	cout << "Sorted List: [ ";
+	for (int i = 0; i < size; i++)
+		cout << array[i] << " ";
+
+	cout << "]" << endl;
+
+
+	system("pause");
+}
+
