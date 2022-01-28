f2a6172ec7b6c053dcca39a43f2be0e74aa2fe19
Syed Shah Meer Haider <60551634+CyBeR1337x@users.noreply.github.com>
2022-01-28 23:57:12 +0500

    Create TASK_20.1(PG_90).cpp

---
+++ TASK_20.1(PG_90).cpp
@@ -0,0 +1,29 @@
+/*
+ IDK IF THIS IS THE DESIRED SOLUTION OR NOT MAKE SURE YOU READ STATEMENT BY YOURSELF.. (C: SHAH MEER HAIDER)
+*/
+#include <iostream>
+using namespace std;
+
+void main() {
+	int size;
+	cout << "Enter no. of courses Studied: ";
+	cin >> size;
+	float* obtained_quality_points = new float[size];
+	float* credit_hour = new float[size];
+
+	cout << "Enter Credit Hour & Quality points: ";
+	for (int i = 0; i < size; i++)
+		cin >> credit_hour[i] >> obtained_quality_points[i];
+
+	int sum_obt_points = 0, sum_credit_hours = 0;
+	for (int i = 0; i < size; i++) {
+		sum_obt_points = obtained_quality_points[i];
+		sum_credit_hours = credit_hour[i];
+	}
+
+	float cgpa = sum_obt_points / sum_credit_hours;
+
+	cout << "Your CGPA: " << cgpa << endl;
+
+	system("pause");
+}
