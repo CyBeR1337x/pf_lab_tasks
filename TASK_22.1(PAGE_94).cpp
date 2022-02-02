4978f7ba9fec5ed9d68b7f1dcd448d366eefe340
Syed Shah Meer Haider <60551634+CyBeR1337x@users.noreply.github.com>
2022-02-02 15:22:53 +0500

    Update TASK_22.1(PAGE_94).cpp

--- TASK_22.1(PAGE_94).cpp
+++ TASK_22.1(PAGE_94).cpp
@@ -6,48 +6,49 @@
 #include <iostream>
 using namespace std;
 
-void main() {
-	char* arid_no;
-	int size;
-	cout << "Enter size of array: ";
-	cin >> size;
+int main() {
+    char* arid_no;
+    int size;
+    cout << "Enter size of array: ";
+    cin >> size;
 
-	arid_no = new char[size];
-	cout << "Enter ARID Number: ";
-	cin >> arid_no;
+    arid_no = new char[size];
+    cout << "Enter ARID Number: ";
+    cin >> arid_no;
 
-	int valid_name = 0;
+    int valid_id = 0;
 
-	for (int i = 0; i < 4; i++) {
-		if (arid_no[i] >= '0' && arid_no[i] <= '9')
-			valid_name++;
-	}
-	if (arid_no[4] == '-')
-		valid_name++;
-	if (arid_no[9] == '-')
-		valid_name++;
+    for (int i = 0; i < 4; i++) {
+        if (arid_no[i] >= '0' && arid_no[i] <= '9')
+            valid_id++;
+    }
+    if (arid_no[4] == '-')
+        valid_id++;
+    if (arid_no[9] == '-')
+        valid_id++;
 
-	if (arid_no[5] == 'A')
-		valid_name++;
-	if (arid_no[6] == 'R')
-		valid_name++;
-	if (arid_no[7] == 'I')
-		valid_name++;
-	if (arid_no[8] == 'D')
-		valid_name++;
+    if (arid_no[5] == 'A')
+        valid_id++;
+    if (arid_no[6] == 'R')
+        valid_id++;
+    if (arid_no[7] == 'I')
+        valid_id++;
+    if (arid_no[8] == 'D')
+        valid_id++;
 
-	for (int i = 9; i < 14; i++) {
-		if (arid_no[i] >= '0' && arid_no[i] <= '9')
-			valid_name++;
-	}
-
-
-	if (valid_name == 14)
-		cout << "Arid number is valid.." << endl;
-	else
-		cout << "Arid number is invalid.." << endl;
+    for (int i = 10; i < 14; i++) 
+        if (arid_no[i] >= '0' && arid_no[i] <= '9')
+            valid_id++;
+    
 
 
-	system("pause");
+    if (valid_id == 14)
+        cout << "Arid number is valid.." << endl;
+    else
+        cout << "Arid number is invalid.." << endl;
+
+
+    system("pause");
 
 }
+
